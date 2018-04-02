6c625432838eb594ce54fb36932911a205c1ce60
Lulu Wang <macolulu@gmail.com>
2018-04-02 09:23:56 -0400

    revers a string

--- com/string/ReverseString.java
+++ com/string/ReverseString.java
@@ -1,9 +1,31 @@
-
+import java.lang.StringBuffer;
 
 public class ReverseString{
+	
+	static String reverseStringBuffer(String s){
+		StringBuffer sb = new StringBuffer(s);
+		return sb.reverse().toString();
+	}
+	
+	static void reverseIterative(String s){
+		char[] sArr = s.toCharArray();
+		for(int i=sArr.length-1; i>=0; i--){
+			System.out.print(sArr[i]);
+		}
+		System.out.println();
+	}
+	
+	static String reverseRecursive(String s){
+		if(s== null || s.length()<=1){
+			return s;
+		}
+		
+		return reverseRecursive(s.substring(1))+s.charAt(0);
+	}
 
 	public static void main(String[] args){
-
-		System.out.println("ok");
+		//System.out.println(reverseStringBuffer("MyJava"));
+		//reverseIterative("MyJava");
+		System.out.println(reverseRecursive("MyJava"));
 	}
 }
