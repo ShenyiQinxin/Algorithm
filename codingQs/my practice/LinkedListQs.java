e3a30f0d91cda09d497b5bf4cddb33170655165e
Lulu Wang <macolulu@gmail.com>
2018-12-09 09:34:27 -0500

    practice 3

--- codingQs/my practice/LinkedListQs.java
+++ codingQs/my practice/LinkedListQs.java
@@ -24,6 +24,14 @@ public class LinkedListQs {
         return prev;
 	}
 
+	// Example 1:
+
+	// Input: 1->2
+	// Output: false
+	// Example 2:
+
+	// Input: 1->2->2->1
+	// Output: true
 	public static boolean isPalindrome(ListNode head) {
 		if(head == null || head.next == null) {
 			return true;
@@ -46,6 +54,8 @@ public class LinkedListQs {
 		return true;
 	}
 
+	//Input: head = [4,5,1,9], node = 1
+	//Output: [4,5,9]
 	public static ListNode deleteNode(ListNode node) {
 		node.val = node.next.val;
 		System.out.print(node.next.val+" ");
@@ -53,13 +63,60 @@ public class LinkedListQs {
 		return node.next;
 	}
 
-	public ListNode removeElements(ListNode head, int val) {
+	//Input:  1->2->6->3->4->5->6, val = 6
+	//Output: 1->2->3->4->5
+	public static ListNode removeElements(ListNode head, int val) {
+		//remove head
 		while(head!=null && head.val==val) {
             head=head.next;
         }
+        //remove curr.next
+        ListNode curr = head;
+        while(curr != null && curr.next != null) {
+        	if(curr.next.val == val) {
+        		curr.next = curr.next.next;
+        	} else {
+        		curr = curr.next;
+        	}
+        }
 
-		return null;
+		return head;
 	}	
+
+	// Input: 1->2->4, 1->3->4
+    // Output: 1->1->2->3->4->4
+	public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode prehead = new ListNode(-1);
+		ListNode prev = prehead;
+		while(l1 != null && l2 != null) {
+			if(l1.val <= l2.val) {
+				prev.next = l1;
+				l1 = l1.next;
+			} else {
+				prev.next = l2;
+				l2 = l2.next;
+			}
+		}
+		prev.next = l1 == null ? l2 : l1;
+		return prehead.next;
+	}
+
+	//A:          a1 → a2
+    //                  ↘
+    //                    c1 → c2 → c3
+    //                  ↗            
+	//B:     b1 → b2 → b3
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if(headA == null || headB == null) return null;
+		ListNode a = headA, b= headB;
+		while(a != b) {
+			a = (a!=null) ? a.next : headB;
+			b = (b!=null) ? b.next : headA;
+		}
+		return headA;
+	}
+ 
+
 
 	private static ListNode createNodeList(int length) {
 		ListNode head = new ListNode(1);
@@ -76,17 +133,16 @@ public class LinkedListQs {
 		return head;
 	}
 
-
-
 	public static void main(String[] args) {
 		ListNode head = createNodeList(9);
 		//System.out.println(middleNode(head).val);
 		//System.out.println(isPalindrome(head));
-		ListNode node = deleteNode(head);
-		while(head != null) {
-			System.out.print(node.val+" ");
-			node = node.next;
-		}
+		// ListNode node = deleteNode(head);
+		// while(head != null) {
+		// 	System.out.print(node.val+" ");
+		// 	node = node.next;
+		// }
+		System.out.println(removeElements(head, 6).val);
 	}
 }
 
