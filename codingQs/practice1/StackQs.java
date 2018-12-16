db0603afe97ec9bda69ef32abb526ccce22785fe
Lulu Wang <lwang@vibrenthealth.com>
2018-12-16 09:51:37 -0500

    .

---
+++ codingQs/practice1/StackQs.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class StackQs {
+
+	/*
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+	*/
+
+	private Stack<Integer> stack;
+	private Stack<Integer> minStack;
+
+	public StackQs() {
+		stack = new Stack<>();
+		minStack = new Stack<>();
+	}
+
+	public void push(int x) {
+		stack.push(x);
+		if(minStack.empty() || x <= minStack.peek()) {
+			minStack.push(x);
+		}
+	}
+
+	public void pop() {
+		if(stack.empty()) {
+			throw new IllegalArgumentException();
+		}
+		int item = stack.pop();
+		if(item == minStack.peek()) {
+			minStack.pop();
+		}
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+
+}
---
+++ codingQs/practice2/ArrayQs.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ArrayQs {
+
+	public static int climbStairs(int n) {}
+	public static int fib(int n) {}
+	public static int fibRecursive(int n) {}
+	public static int removeElement(int[] nums, int val) {}
+	public static int removeDuplicates(int[] nums) {}
+	public static int[] moveZeroes(int[] nums) {}
+	public static int[] rotate(int[] nums, int k) {}
+	private static void reverse(int[] nums, int l, int r){}
+	public static int[] plusOne(int[] digits) {}
+	public static int arrayPairSum(int[] nums) {}
+	public int islandPerimeter(int[][] grid) {}
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {}
+	public static boolean isPalindrome(String s0) {}
+
+	public static void main(String[] args) {
+
+	}
+}
