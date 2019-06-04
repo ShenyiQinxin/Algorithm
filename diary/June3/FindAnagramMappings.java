87f35e58fcab192c9545792adc86c4654eb2a03f
Lulu Wang <macolulu@gmail.com>
2019-06-03 22:34:03 -0400

    wip

--- diary/June3/FindAnagramMappings.java
+++ diary/June3/FindAnagramMappings.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * A = [12, 28, 46, 32, 50]
  *
@@ -6,12 +8,24 @@
  * We should return [1, 4, 3,2, 0]
  */
 class FindAnagramMappings {
-    public int[] anagramMappings(int[] A, int[] B) {
-
+    static int[] anagramMappings(int[] A, int[] B) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < B.length; i++) {
+            map.put(B[i], i);
+        }
+        int[] res = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            res[i] = map.get(A[i]);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 3, 0 };
-        System.out.println(containsDuplicate(nums));
+        int[] A = { 12, 28, 46, 32, 50 };
+        int[] B = { 50, 12, 32, 46, 28 };
+        int[] nums = anagramMappings(A, B);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
     }
 }
