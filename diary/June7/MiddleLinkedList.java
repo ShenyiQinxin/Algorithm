19e762c4ba2b820e10b61fa24ddedca8d936a512
Lulu Wang <lwang@vibrenthealth.com>
2019-06-07 15:37:17 -0400

    6/7

--- diary/June7/MiddleLinkedList.java
+++ diary/June7/MiddleLinkedList.java
@@ -28,12 +28,14 @@ public class MiddleLinkedList {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
         ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
+        // ListNode node4 = new ListNode(4);
         head.next = node1;
         node1.next = node2;
         node2.next = node3;
-        node3.next = node4;
-        System.out.println(head.val + " " + head.next.val + " " + head.next.next.val + " " + head.next.next.next.val
-                + " " + head.next.next.next.next.val);
+        // node3.next = node4;
+        System.out.println(middleNode(head).val);
+        // System.out.println(head.val + " " + head.next.val + " " + head.next.next.val
+        // + " " + head.next.next.next.val
+        // + " " + head.next.next.next.next.val);
     }
 }
