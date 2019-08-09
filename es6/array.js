27ddb4a71f4e9544633082a10cfde7e65bc21834
Lulu Wang <macolulu@gmail.com>
2019-08-09 09:56:46 +0900

    changes in es6

---
+++ es6/array.js
@@ -0,0 +1,13 @@
+/**
+ * forEach map filter find every some reduce
+ */
+var colors = ['red', 'blue', 'green'];
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+
+}
+
+
+colors.forEach(function (color) {
+    console.log(color);
+});
